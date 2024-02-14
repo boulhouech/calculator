9d3f0a94e418d7cf436cd8e223ab8e280437db2d
boulhouech <slim.boulahouech@gmail.com>
2024-02-14 21:55:40 +0100

    Set up JavaScript to use the first function that displays the clicked operator in the operation div

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const displayOperation = document.querySelector('.operation');
 const displayResult = document.querySelector('.result');
-let displayValue = ""; 
+displayOperation.textContent ="0";
+let displayValue = '';
+
 
 function appendNumber (number) {
     displayValue = number;
     document.querySelector('.operation').innerHTML = displayValue; 
+}
+
+function performOperation (operator) {
+    displayValue = operator;
+    document.querySelector('.operation').innerHTML = displayValue;
 }
 
