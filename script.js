f321ee9655ae1bc95ea5c3a20e3c55b143325db6
boulhouech <slim.boulahouech@gmail.com>
2024-02-15 01:35:59 +0100

    creating operate function and renaming the operation functions and including a condition in the divide function to prevent division by zero

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
-const displayOperation = document.querySelector('.operation');
-const displayResult = document.querySelector('.result');
-displayOperation.textContent ="0";
-let displayValue = '';
+    const displayOperation = document.querySelector('.operation');
+    const displayResult = document.querySelector('.result');
+    displayOperation.textContent ="0";
+    let displayValue = '';
 
-function updateDisplay () {
-    displayOperation.innerHTML = displayValue;
-}
+    function updateDisplay () {
+        displayOperation.innerHTML = displayValue;
+    }
 
-function appendNumber (number) {
-    displayValue += number;
-    updateDisplay();
-}
+    function appendNumber (number) {
+        displayValue += number;
+        updateDisplay();
+    }
 
-function performOperation (operator) {
-    displayValue += operator;
-    updateDisplay();
-}
+    function performOperation (operator) {
+        displayValue += ' ' + operator + ' ';
+        updateDisplay();
+    }
 
-function clearEntry () {
-    displayValue = "";
-    updateDisplay();
-}
+    function clearEntry () {
+        displayValue = "";
+        updateDisplay();
+    }
 
-let adding = (firstValue, secondValue) => {
-    return firstValue + secondValue;
-}
+    let addNumbers = (firstValue, secondValue) => {
+        return firstValue + secondValue;
+    }
 
-let substracting = (firstValue, secondValue) => {
-    return  firstValue - secondValue;
-}
+    let subtractNumbers = (firstValue, secondValue) => {
+        return  firstValue - secondValue;
+    }
 
-let multiply = (firstValue, secondValue) => {
-    return firstValue * secondValue;
-}
+    let multiplyNumbers = (firstValue, secondValue) => {
+        return firstValue * secondValue;
+    }
 
-let division = (firstValue, secondValue)  => {
-    return firstValue / secondValue;
-}
+    let divideNumbers = (firstValue, secondValue)  => {
+        if (num2 === 0) {
+            return "Error: Division by zero!";
+        } else {
+            return firstValue / secondValue;
+        }
+    }
+    
+
+    function operate(operator, firstValue, secondValue) {
+        switch (operate) {
+            case '+':
+                return addNumbers(firstValue, secondValue);
+            case '-':
+                return subtractNumbers(firstValue, secondValue);    
+            case '*':
+                return multiplyNumbers(firstValue, secondValue); 
+            case '/': 
+                return divideNumbers(firstValue, secondValue);
+        }   
+        
+    }  
+
+
 
 
