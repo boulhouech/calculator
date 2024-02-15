e58fd39412bbea4dce9cb81dde5df33cda75236f
boulhouech <slim.boulahouech@gmail.com>
2024-02-15 16:57:41 +0100

    update the code to make more operations based on the previous result, and fix the problem in which the letter 'c' is being displayed whenever the button c is clicked

--- script.js
+++ script.js
@@ -1,113 +1,123 @@
 const displayOperation = document.querySelector('.operation');
-const displayResult = document.querySelector('.result');
-let displayValue = '';
-let resultBeingDisplayed = '';
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-displayOperation.textContent = "0";
+        const displayResult = document.querySelector('.result');
+        let displayValue = '';
+        let resultBeingDisplayed = '';
+        let firstNumber = '';
+        let secondNumber = '';
+        let operator = '';
+        displayOperation.textContent = "0";
 
 
-function populateResult () {
-    displayResult.innerHTML = resultBeingDisplayed;
+        function populateResult() {
+            displayResult.innerHTML = resultBeingDisplayed;
 
-}
+        }
 
-function populateDisplay() {
-    displayOperation.innerHTML = displayValue;
-}
+        function populateDisplay() {
+            displayOperation.innerHTML = displayValue;
+        }
 
-function appendNumber(number) {
-    displayValue += number;
-    populateDisplay();
-}
+        function appendNumber(number) {
+            displayValue += number;
+            populateDisplay();
+        }
 
-function performOperation(op) {
-    displayValue += ' ' + op + ' ';
-    populateDisplay();
-}
+        function performOperation(op) {
+            displayValue += ' ' + op + ' ';
+            populateDisplay();
+        }
 
-function clearEntry() {
-    displayValue = "";
-    firstNumber = '';
-    secondNumber = '';
-    operator = '';
-    populateDisplay();
-}
+        function clearEntry() {
+            displayValue = "";
+            firstNumber = '';
+            secondNumber = '';
+            operator = '';
+            resultBeingDisplayed = '';
+            populateDisplay();
+            populateResult();
+        }
 
-let addNumbers = (firstValue, secondValue) => {
-    return firstValue + secondValue;
-}
+        let addNumbers = (firstValue, secondValue) => {
+            return firstValue + secondValue;
+        }
 
-let subtractNumbers = (firstValue, secondValue) => {
-    return firstValue - secondValue;
-}
+        let subtractNumbers = (firstValue, secondValue) => {
+            return firstValue - secondValue;
+        }
 
-let multiplyNumbers = (firstValue, secondValue) => {
-    return firstValue * secondValue;
-}
+        let multiplyNumbers = (firstValue, secondValue) => {
+            return firstValue * secondValue;
+        }
 
-let divideNumbers = (firstValue, secondValue) => {
-    if (secondValue === 0) {
-        return "Error: Division by zero!";
-    } else {
-        return firstValue / secondValue;
-    }
-}
+        let divideNumbers = (firstValue, secondValue) => {
+            if (secondValue === 0) {
+                return "Error: Division by zero!";
+            } else {
+                return firstValue / secondValue;
+            }
+        }
 
-function operate(selectedOperator, firstValue, secondValue) {
-    switch (selectedOperator) {
-        case '+':
-            return addNumbers(firstValue, secondValue);
-        case '-':
-            return subtractNumbers(firstValue, secondValue);
-        case '*':
-            return multiplyNumbers(firstValue, secondValue);
-        case '/':
-            return divideNumbers(firstValue, secondValue);
-    }
-}
+        function operate(selectedOperator, firstValue, secondValue) {
+            switch (selectedOperator) {
+                case '+':
+                    return addNumbers(firstValue, secondValue);
+                case '-':
+                    return subtractNumbers(firstValue, secondValue);
+                case '*':
+                    return multiplyNumbers(firstValue, secondValue);
+                case '/':
+                    return divideNumbers(firstValue, secondValue);
+            }
+        }
 
-document.addEventListener('click', (event) => {
-    const clickedElement = event.target;
+        document.addEventListener('click', (event) => {
+            const clickedElement = event.target;
 
-    if (clickedElement.classList.contains('digit')) {
-        handleNumberClicked(clickedElement.innerHTML);
-    } else if (clickedElement.classList.contains('operation')) {
-        handleOperatorClicked(clickedElement.innerHTML);
-    } else if (clickedElement.classList.contains('equal-button')) {
-        performCalculation();
-    } else if (clickedElement.classList.contains('clear-button')) {
-        clearEntry();
-    }
-});
+            if (clickedElement.classList.contains('digit')) {
+                handleNumberClicked(clickedElement.innerHTML);
+            } else if (clickedElement.classList.contains('operation')) {
+                handleOperatorClicked(clickedElement.innerHTML);
+            } else if (clickedElement.classList.contains('equal-button')) {
+                performCalculation();
+            } else if (clickedElement.classList.contains('clear-button')) {
+                clearEntry();
+            }
+        });
 
-function handleNumberClicked(number) {
-    if (operator === '') {
-        firstNumber += number;
-        updateDisplay();
-    } else {
-        secondNumber += number;
-        updateDisplay();
-    }
-}
+        function handleNumberClicked(number) {
+            if (operator === '') {
+                firstNumber += number;
+                updateDisplay();
+            } else {
+                secondNumber += number;
+                updateDisplay();
+            }
+        }
 
-function handleOperatorClicked(op) {
-    if (firstNumber !== '' && secondNumber !== '') {
-        performCalculation();
-    }
-    operator = op;
-}
+        function handleOperatorClicked(op) {
+            if (firstNumber !== '' && secondNumber !== '') {
+                performCalculation();
+                // After calculation, reset secondNumber
+                secondNumber = '';
+            }
+            operator = op;
+        }
 
-function updateDisplay() {
-    displayValue = `${firstNumber} ${operator} ${secondNumber}`;
-    populateDisplay();
-}
 
-function performCalculation() {
-    const result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
-    displayValue = result.toString();
-    resultBeingDisplayed = displayValue;
-    populateResult()
-    populateDisplay();
-}
+        function updateDisplay() {
+            displayValue = `${firstNumber} ${operator} ${secondNumber}`;
+            populateDisplay();
+        }
+
+        function performCalculation() {
+            const result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+            resultBeingDisplayed = result.toString();
+
+            // Update variables for subsequent operations
+            firstNumber = resultBeingDisplayed;
+            secondNumber = '';
+            operator = '';
+
+            populateResult();
+            populateDisplay();
+        }
